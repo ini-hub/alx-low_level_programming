6e4bf3ae20d561fc70b90b9a390228ca9c44c7bb
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-16 18:37:51 -0800

    k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <unistd>
+#include <unistd.h>
 /**
  * main - A program that prints followed by a new line, to the standard error
  * Return: 1 (Success)
@@ -7,5 +7,5 @@
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	return (1);
 }
