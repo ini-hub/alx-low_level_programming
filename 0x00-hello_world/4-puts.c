e6e7316599242947f8df220ea4dd606a54318f8c
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-16 17:25:01 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is a program that uses puts function to print out a
+ * string to screen
+ * return - 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
