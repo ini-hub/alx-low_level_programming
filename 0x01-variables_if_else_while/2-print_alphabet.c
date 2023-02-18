4977126948761db1fcf6b50be495def190edf8d9
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-17 17:30:42 -0800

    2-print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: 0 (Success)
+/* betty style doc for function main goes there */
+int main(void)
+{
+	Char alp[26] = "abcdefghijkImnopqrstuvwxyz";
+	int i;
+		for (i = 0; i < 26; i++)
+		{
+			putchar(alp[1]);
+		}
+		putchar("\n");
+	return (0);
+}
