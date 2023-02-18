e6ebae9ab2b63ab344b0252b1156902f29d7a786
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-17 18:00:33 -0800

    h

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ * Return: 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	return (0);
+}
