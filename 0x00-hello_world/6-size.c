3a4e3104cf33f41bd258fd026afecde63e7b8547
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-18 19:56:44 -0800

    stderr3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	float f;
 
 printf("Size of a char: %ld byte(s)\n", sizeof(a));
-printf("Size of a int: %ld byte(s)\n", sizeof(b));
+printf("Size of an int: %ld byte(s)\n", sizeof(b));
 printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 printf("Size of a float: %ld byte(s)\n", sizeof(f));
-fprintf(stderr, "%s\n", "");
+fprintf(stderr, "%s", "");
 return (0);
 }
