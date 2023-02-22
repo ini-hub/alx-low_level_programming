d0780cf54c4cb37076216f35ba061d7bf60295ec
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-21 16:24:52 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that computes and prints the sum of all the multiples (3|5)
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	int i;
+	int sum;
+	sum = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
