f5103ac9a1bcb4511c5eb715f4c2a2dde260b783
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-22 21:06:35 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,6 +3,7 @@
 /**
  * print_to_98 - A function that prints all natural numbers from n to 98
  * Return: 0 Always.
+ * @n: function parameter
  */
 void print_to_98(int n)
 {
@@ -12,9 +13,11 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+				printf("%d\n", i);
+			else 
+				printf("%d, \n", i);
 		}
-		printf("\n");
 	}
 	else if (n > 98)
 	{
@@ -26,12 +29,12 @@ void print_to_98(int n)
 	}
 	else if (n < 0)
 	{
-		for (i = n ; 1 <= 98 ; i++)
+		for (i = n ; i <= 98 ; i++)
 		{
 			printf("%d, ", i);
 		}
 		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d\n", n);
 }
