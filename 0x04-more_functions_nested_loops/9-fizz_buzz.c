51947e25c610a79bac40f5fbf559c0f36dd9b102
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-23 21:26:49 -0800

    buzzzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,23 @@ int main(void)
 	{
 		if ((num % 3 == 0) && (num % 15 != 0))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((num % 5 == 0) && (num % 15 != 0))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (num % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", num);
+		}
+		if (num != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
