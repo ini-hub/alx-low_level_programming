09d4a318fc806b4e3b7ebc6384e376abd33a3059
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-23 20:36:01 -0800

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - A function that prints a square
+ * Return: 0 Always
+ */
+
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
