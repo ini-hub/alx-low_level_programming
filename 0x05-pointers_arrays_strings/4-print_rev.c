b48e9165bd4c26e4a9030ca4a94233deda2ef402
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-27 15:51:01 -0800

    i reversed it

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Afunction that prints a string, followed by a new line
+ * @s: Function parameter
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int len = 0, i;
+
+	/* Find the length of the string */
+	while (s[len] != '\0')
+		len++;
+
+	/* Print the string in reverse order */
+	for (i = len - 1; i >= 0; i--)
+		putchar(s[i]);
+
+	/* Print a new line at the end */
+	putchar('\n');
+}
