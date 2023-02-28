e6daa3bf93d3949803630a9caa0e83010eb1ec5d
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-28 03:59:30 -0800

    hmm

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * puts2 - prints every other character of a string, 
+ * puts2 - prints every other character of a string,
  * starting with the first character
  * @str: the string to print
  */
