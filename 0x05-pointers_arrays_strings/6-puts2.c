725d45a657183c418e5006b7f5dddd85abfac376
Bolujo Excellent <ebolujo101@gmail.com>
2023-02-28 03:56:34 -0800

    look

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string, 
+ * starting with the first character
  * @str: the string to print
  */
 void puts2(char *str)
