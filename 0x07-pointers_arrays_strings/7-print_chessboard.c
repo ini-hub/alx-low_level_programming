0f740c07fff4eebc00de4a142cd33331e44934e9
Bolujo Excellent <ebolujo101@gmail.com>
2023-03-06 21:02:31 -0800

    all 1

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *print_chessboeard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+
+void print_chesssboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
