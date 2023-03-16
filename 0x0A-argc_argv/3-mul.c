2371c560416e8412fbddbdd652a6891d947fd22b
Bolujo Excellent <ebolujo101@gmail.com>
2023-03-15 19:05:53 -0700

    task 3-5

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Prints the number of args
@@ -9,11 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int n1 = 0, n2 = 0;
 
-	for (i = 0; i < argc; i++)
+	if (argc == 3)
 	{
-		printf("%s\n", argv[i]);
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
